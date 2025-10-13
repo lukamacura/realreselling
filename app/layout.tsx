07af36ae833fd74884c546aae92ba28d9869d73c
lukamacura <designmacura@gmail.com>
2025-10-13 13:49:49 +0200

    Meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import "./globals.css";
 import { Inter, Bebas_Neue } from "next/font/google";
 import type { Metadata, Viewport } from "next";
-import PixelTracker from "../components/PixelTracker"; // ← regular import
+import Script from "next/script";
+import PixelTracker from "../components/PixelTracker"; // ⬅ direktan import klijentske komponente
 
 export const metadata: Metadata = {
   title: "Real Reselling",
@@ -19,8 +21,33 @@ const bebas = Bebas_Neue({ weight: "400", subsets: ["latin"], variable: "--font-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="sr" className="dark">
+      <head>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '622346010959688');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=622346010959688&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
       <body className={`${inter.variable} ${bebas.variable} font-sans bg-brand-dark text-white overflow-x-hidden`}>
-        <PixelTracker />   {/* fine to render a client child in a server layout */}
+        <PixelTracker /> {/* klijentska komponenta, bez dynamic/ssr:false */}
         {children}
       </body>
     </html>
--- components/PixelTracker.tsx
+++ components/PixelTracker.tsx
@@ -1,27 +1,14 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import ReactPixel from "react-facebook-pixel";
-import { usePathname, useSearchParams } from "next/navigation";
 
 export default function PixelTracker() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const initialized = useRef(false);
-
   useEffect(() => {
-    const pixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || "YOUR_PIXEL_ID";
-    if (!pixelId) return;
-    if (!initialized.current) {
-      ReactPixel.init(pixelId, undefined, { autoConfig: true, debug: false });
-      initialized.current = true;
-    }
-    ReactPixel.pageView(); // initial
+    const pixelId = process.env.NEXT_PUBLIC_FB_PIXEL_ID || "622346010959688";
+    ReactPixel.init(pixelId);
+    ReactPixel.pageView();
   }, []);
-
-  useEffect(() => {
-    if (initialized.current) ReactPixel.pageView();
-  }, [pathname, searchParams]);
 
   return null;
 }
