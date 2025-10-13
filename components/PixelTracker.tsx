dd1ee91bfac730665b02d44e18ab744b03170f92
lukamacura <designmacura@gmail.com>
2025-10-13 13:54:11 +0200

    Meta pixel

--- components/PixelTracker.tsx
+++ components/PixelTracker.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { useEffect } from "react";
-import ReactPixel from "react-facebook-pixel";
 
 export default function PixelTracker() {
   useEffect(() => {
-    const pixelId = process.env.NEXT_PUBLIC_FB_PIXEL_ID || "622346010959688";
-    ReactPixel.init(pixelId);
-    ReactPixel.pageView();
+    if (typeof window === "undefined") return; // SSR guard
+
+    (async () => {
+      const mod = await import("react-facebook-pixel");
+      const ReactPixel = mod.default;
+
+      const pixelId = process.env.NEXT_PUBLIC_FB_PIXEL_ID || "622346010959688";
+      ReactPixel.init(pixelId);
+      ReactPixel.pageView();
+    })();
   }, []);
 
   return null;
